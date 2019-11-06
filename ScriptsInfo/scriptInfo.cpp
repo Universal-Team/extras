866d0846478a7ba766c91bdf30ea635ef2bddad8
Pk11 <epicpkmn11@outlook.com>
2019-11-06 01:27:56 -0600

    Update scriptInfo.cpp

--- ScriptsInfo/scriptInfo.cpp
+++ ScriptsInfo/scriptInfo.cpp
@@ -55,9 +55,8 @@ nlohmann::json readInfo(const std::string &path) {
 		if(in["info"].contains("author") && in["info"]["author"].is_string())	out["author"] = in["info"]["author"];
 		if(in["info"].contains("version") && in["info"]["version"].is_number())	out["version"] = in["info"]["version"];
 		if(in["info"].contains("revision") && in["info"]["revision"].is_number())	out["revision"] = in["info"]["revision"];
-
-		out["url"] = replaceSpaces("https://github.com/Universal-Team/extras/raw/master/Scripts"+path.substr(path.find_last_of("/")));
 	}
+	out["url"] = replaceSpaces("https://github.com/Universal-Team/extras/raw/master/Scripts"+path.substr(path.find_last_of("/")));
 
 	return out;
 }
